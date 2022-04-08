bd2abe808d879251964e5494c2ccdf1997311024
Tripat Kaur <tripat.kaur6@gmail.com>
2022-04-08 08:14:57 +0200

    ScrumBoard UI
    1. Added rows and columns from antd to display the status lists properly

--- src/Components/ScrumBoard.js
+++ src/Components/ScrumBoard.js
@@ -1,52 +1,71 @@
-import React,{useState,useEffect} from 'react'
-import { v4 as uuidv4 } from 'uuid'
-import StatusList from './StatusList'
+import React, { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+import StatusList from "./StatusList";
+import { Layout, Row, Col, Typography } from "antd";
+import "antd/dist/antd.css";
 
+const { Content } = Layout;
+
+const { Title } = Typography;
 
 const data = {
-    [uuidv4()]:{
-        title:"To-Do",
-        tasks:[{
-            id:uuidv4(),
-            content:"To-Do1"
-        },{
-            id:uuidv4(),
-            content:"To-Do2"
-        },{
-            id:uuidv4(),
-            content:"To-Do3"
-        }]
-    },
-    [uuidv4()]:{
-        title:"In Progress",
-        tasks:[]
-    },
-    [uuidv4()]:{
-        title:"Done",
-        tasks:[]
-    }
-}
+  [uuidv4()]: {
+    title: "Story",
+    tasks: [
+      {
+        id: uuidv4(),
+        content: "To-Do1",
+      },
+      {
+        id: uuidv4(),
+        content: "To-Do2",
+      },
+      {
+        id: uuidv4(),
+        content: "To-Do3",
+      },
+    ],
+  },
+  [uuidv4()]: {
+    title: "Not Started",
+    tasks: [],
+  },
+  [uuidv4()]: {
+    title: "In Progress",
+    tasks: [],
+  },
+  [uuidv4()]: {
+    title: "Done",
+    tasks: [],
+  },
+};
 
+const ScrumBoard = () => {
+  const [completeTaskList, setCompleteTaskList] = useState({});
 
-
-const ScrumBoard = () =>  {
-    const [completeTaskList,setCompleteTaskList] = useState({})
-    
-    useEffect(()=>{
-        setCompleteTaskList(data)
-    },[])
   
-    return (
-        <div>
-            ScrumBoard
-            {
-                Object.keys(completeTaskList).map((listId)=>{
-                    return <StatusList key={listId} statusObj={completeTaskList[listId]}/>
-                })
-            }
 
-        </div>
-    )
-}
+  useEffect(() => {
+    setCompleteTaskList(data);
+  }, []);
 
-export default ScrumBoard
+  return (
+    <Content>
+      <Row justify="center">
+        <Title level={3}>ScrumBoard</Title>
+      </Row>
+
+      <Row justify="space-between">
+        {Object.keys(completeTaskList).map((listId) => {
+          return (
+            <Col  span={5} key={listId} style={{ background: "lightgrey" }}>
+              <StatusList key={listId} statusObj={completeTaskList[listId]} />
+            </Col>
+          );
+        })}
+      </Row>
+    </Content>
+  );
+};
+
+export default ScrumBoard;
